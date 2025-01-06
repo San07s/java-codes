c351a4780f32508b1c40430cf91b26201fc973cd
santhosh <155651116+San07s@users.noreply.github.com>
2025-01-06 10:55:56 +0530

    Add files via upload

---
+++ foreach.java
@@ -0,0 +1,17 @@
+// For-each Loop
+
+class foreach
+{
+	public static void main(String []sp)
+	{
+		int a[]={1,2,3,4,5,6,7,8,9,10};
+		
+		for (int i:a)
+		{
+			System.out.println(i);
+		}
+	}
+}
+
+// This is use to when someone ask how to traverse array without 
+//using length function that time use for each loop.
