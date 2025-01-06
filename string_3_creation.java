392841b1ff35392e68c73384f999ee78aba64e43
santhosh <155651116+San07s@users.noreply.github.com>
2025-01-06 12:27:25 +0530

    Add files via upload

---
+++ string_3_creation.java
@@ -0,0 +1,26 @@
+/*
+This program creates 3 way of String creations.
+
+You wannna create String first you create object Here, 3 ways of object creations
+<String>.<StringBuffer>.<StringBuilder>
+
+String you can create with or without using new Keyword.
+StringBuffer you can only create using new Keyword.
+StringBuilder you can only create using new Keyword.
+*/
+
+class string_3_creation
+{
+	public static void main(String []sp)
+	{
+		String s1="Santhosh";
+		String s2=new String("Santhosh");
+		StringBuffer s3=new StringBuffer("Santhosh");
+		StringBuilder s4=new StringBuilder("Santhosh");
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		System.out.println(s4);
+	}
+}
